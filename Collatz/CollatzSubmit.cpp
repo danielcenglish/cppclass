a3153e4923e355e20cd29bf4cf05ad59620bbf97
Daniel English <daniel.c.english@gmail.com>
2015-09-27 20:29:32 -0500

    Working "dumb solution" of Collatz

--- Collatz/CollatzSubmit.cpp
+++ Collatz/CollatzSubmit.cpp
@@ -1,12 +1,3 @@
-// ----------------------------
-// projects/collatz/Collatz.c++
-// Copyright (C) 2015
-// Glenn P. Downing
-// ----------------------------
-
-// --------
-// includes
-// --------
 
 #include <cassert>  // assert
 #include <iostream> // endl, istream, ostream
@@ -14,9 +5,58 @@
 #include <string>   // getline, string
 #include <utility>  // make_pair, pair
 
-#include "Collatz.h"
-
 using namespace std;
+
+// ------------
+// collatz_read
+// ------------
+
+/**
+ * read two ints
+ * @param s a string
+ * @return a pair of ints, representing the beginning and end of a range, [i, j]
+ */
+pair<int, int> collatz_read (const string& s);
+
+// ------------
+// collatz_eval
+// ------------
+
+/**
+ * @param i the beginning of the range, inclusive
+ * @param j the end       of the range, inclusive
+ * @return the max cycle length of the range [i, j]
+ */
+int collatz_eval (int i, int j);
+
+// -------------
+// collatz_print
+// -------------
+
+/**
+ * print three ints
+ * @param w an ostream
+ * @param i the beginning of the range, inclusive
+ * @param j the end       of the range, inclusive
+ * @param v the max cycle length
+ */
+void collatz_print (ostream& w, int i, int j, int v);
+
+// -------------
+// collatz_solve
+// -------------
+
+/**
+ * @param r an istream
+ * @param w an ostream
+ */
+void collatz_solve (istream& r, ostream& w);
+
+int main () {
+    using namespace std;
+    collatz_solve(cin, cout);
+    return 0;}
+
 
 // ------------
 // collatz_read
