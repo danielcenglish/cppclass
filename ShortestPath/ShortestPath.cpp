bea2cbefa4e4ed26ee276a567d4aec8844a1857a
Daniel English <daniel.c.english@gmail.com>
2015-10-12 22:44:35 -0500

    shortest path works, but too slow.

--- ShortestPath/ShortestPath.cpp
+++ ShortestPath/ShortestPath.cpp
@@ -16,6 +16,7 @@ public:
     //priority_queue<int,std::vector<int>,greater<int>> neighbors;
     map<int,int> _neighbors; //neighbor, distance
     int _dist_from_source;
+    int prev_vertex = -1;
     
     Node()
     {
@@ -26,6 +27,16 @@ public:
     {
         _vertexNum = v; //set vertex
         //cout << "Creating node with vertex " << v << endl;
+    }
+    
+    int get_prev()
+    {
+        return prev_vertex;
+    }
+    
+    void set_prev(int p)
+    {
+        prev_vertex = p;
     }
     
     void setVertex(int v)
@@ -104,8 +115,9 @@ map<int,Node> nodes;
 
 //std::priority_queue<int, std::vector<int>, std::greater<int> >third (myints,myints+4);
 
-priority_queue<Node, vector<Node>,mycomparison> unvisited_nodes;
+priority_queue<Node*, vector<Node>,mycomparison> unvisited_nodes;
 
+vector<int> nodes_to_check;
 
 int main()
 {
@@ -119,26 +131,57 @@ int main()
         this_node.setVertex(current_node);
         this_node.setNeighbor(current_target,current_distance);
         
+        Node& other_node = nodes[current_target];
+        other_node.setVertex(current_target);
+        other_node.setNeighbor(current_node,current_distance);
+        
        // unvisited_nodes.push(this_node);
         
     }
     
+    //add final node as a Node object. it won't have any neighbors
     Node& end_node = nodes[end_vertex];
     end_node.setVertex(end_vertex);
     
-    //add nodes to queue
+    
+    //add nodes to a list of nodes to check
     for (map<int,Node>::iterator it=nodes.begin(); it!=nodes.end(); ++it)
     {
-        unvisited_nodes.push(it->second);
+        //unvisited_nodes.push(it->second);
+        nodes_to_check.push_back(it->first);
     }
     
-    while(!unvisited_nodes.empty())
+    while(!nodes_to_check.empty())
     {
-        cout << "Node " << unvisited_nodes.top()._vertexNum << " with distance " << unvisited_nodes.top().distance_from_source() << endl;
+        //find node with least distance from source
+        Node least_node;
+        int least_dist = INT_MAX;
+        vector<int>::iterator index;
+        for(vector<int>::iterator node_it=nodes_to_check.begin(); node_it!=nodes_to_check.end();++node_it)
+        {
+            int cur_dist = nodes[*node_it].distance_from_source();
+            //cout << "Node " << *node_it << " distance " << nodes[*node_it].distance_from_source() << endl;
+            if(cur_dist <= least_dist)
+            {
+                least_node = nodes[*node_it];
+                least_dist = cur_dist;
+                index = node_it;
+            }
+        }
+
+        Node check_node = least_node;
+        if(*index == end_vertex)
+        {
+            nodes_to_check.clear();
+            break;
+        }
+        else
+        {
+            nodes_to_check.erase(index);
+        }
         
-        Node check_node = unvisited_nodes.top();
+        //cout << "Checking node " << check_node._vertexNum << endl;
         
-        unvisited_nodes.pop();
         
         //for each neighbor of check_node
         //measure distance of check_node + distance to neighbor
@@ -147,15 +190,48 @@ int main()
         for(map<int,int>::iterator it=check_node._neighbors.begin(); it!=check_node._neighbors.end();++it)
         {
             int possible_dist = check_node.distance_from_source()+it->second;
+            //cout << "current node distance from source is " << nodes[check_node._vertexNum].distance_from_source() << endl;
+            //cout << "neighbor node " << it->first << " distance from current is " << it->second << endl;
             if(possible_dist < nodes[it->first].distance_from_source())
             {
+                //cout << "setting node " << nodes[it->first]._vertexNum << " to distance " << possible_dist << endl;
                 nodes[it->first].set_distance_from_source(possible_dist);
+                nodes[it->first].set_prev(check_node._vertexNum);
             }
         }
         
+
+        //cout << "End of loop" << endl;
+        
     }
     
-    cout << "Shortest distance from node 1 to node 3: " << nodes[3].distance_from_source() << endl;
+   // cout << "Shortest distance from node 1 to node 4: " << nodes[4].distance_from_source() << endl;
+   /// cout << "node 4 previous node " << nodes[4].get_prev() << endl;
+   // cout << "path: " << endl;
+    
+    vector<Node> node_path;
+    Node last_node = end_node;
+    node_path.push_back(end_node);
+    while(last_node.get_prev() != -1)
+    {
+        node_path.push_back(nodes[last_node.get_prev()]);
+       /// cout << "added node " << last_node.get_prev() << " to path " << endl;
+        last_node = nodes[last_node.get_prev()];
+    }
+    
+    if(node_path.size()>1)
+    {
+        for(vector<Node>::iterator it =node_path.end()-1; it>=node_path.begin(); --it)
+        {
+            cout << it->_vertexNum << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "-1" << endl;
+    }
+    
     
     //go through nodes and set shortest path
     
