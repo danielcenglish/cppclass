0d211079d4d7d157d4eb139109d18fd128f2d12f
Daniel English <daniel.c.english@gmail.com>
2015-10-26 21:42:12 -0500

    almost works, fails on test 13

--- LongestPath/LongestPath.cpp
+++ LongestPath/LongestPath.cpp
@@ -7,14 +7,15 @@
 #include <algorithm>
 #include <map>
 #include <string.h>
+#include <math.h>
 using namespace std;
 
 int main()
 {
     int num_rooms;
-    vector<unsigned int> room_visits; //store number of times a room has been visited
-    vector<unsigned int> destinations; //store destination of second portal for a given room
-    map<int,int>room_cache; //store number of moves it takes to get to room i
+    vector<unsigned long long int> room_visits; //store number of times a room has been visited
+    vector<unsigned long long int> destinations; //store destination of second portal for a given room
+    map<int,long double>room_cache; //store number of moves it takes to get to room i
     
     //Read number of total rooms
     cin >> num_rooms;
@@ -32,30 +33,26 @@ int main()
     int old_room = 0;
 
     int room_to_cache = 0;
-    int num_moves_before_cache = 0;
+    long double num_moves_before_cache = 0;
     
-    unsigned int num_moves = 0; //store the number of moves required
+    long double num_moves = 0; //store the number of moves required
     
     while(current_room != (num_rooms+1)) //Trying to get to the final room
     {
         //any time a room is entered, increment the room_visits for that room.
         ++room_visits[current_room];
         
-        cout << "visiting room " << current_room << " with number of visits: " << room_visits[current_room] << endl;
+       // cout << "Move " << num_moves << ": "<< "visiting room " << current_room << " with number of visits: " << room_visits[current_room] << endl;
         
         //check to see if the current room is cached.
-        map<int,int>::iterator it;
+        map<int,long double>::iterator it;
         it = room_cache.find(current_room);
         if(it == room_cache.end() && room_to_cache == 0) //not found
         {
             //save current state
             room_to_cache = current_room;
             num_moves_before_cache = num_moves;
-            cout << "num moves before cache: " << num_moves_before_cache << endl;
-        }
-        else
-        {
-            cout << "room " << current_room << " is in the cache" << endl;
+            //cout << "num moves before cache: " << num_moves_before_cache << endl;
         }
         
         
@@ -63,11 +60,13 @@ int main()
         if(room_visits[current_room]%2 != 0)
         {
             //first check cache
-            map<int,int>::iterator it;
+            map<int,long double>::iterator it;
             it = room_cache.find(current_room);
             if(it != room_cache.end())
             {
-                cout << "Looking up room " << current_room << " from cache" << endl;
+                //cout << "Looking up room " << current_room << " from cache" << endl;
+                ++room_visits[current_room];
+                //cout << "Room " << current_room << " has " << room_visits[current_room] << " visits." << endl;
                 num_moves += it->second; //add moves
                 ++current_room;
                // ++room_visits[current_room];
@@ -77,9 +76,7 @@ int main()
             
                 old_room = current_room;
                 current_room = destinations[current_room];
-            
-            
-                cout << "going back to room " << current_room << endl;
+                //cout << "going back to room " << current_room << endl;
             }
 
         }
@@ -89,7 +86,7 @@ int main()
             if(current_room == room_to_cache)
             {
                 room_cache[current_room] = num_moves - num_moves_before_cache;
-                cout << "Caching room " << current_room << " as " << num_moves-num_moves_before_cache << " moves." << endl;
+                //cout << "Caching room " << current_room << " as " << num_moves-num_moves_before_cache << " moves." << endl;
                 room_to_cache = 0;
             }
             ++current_room;
@@ -100,34 +97,12 @@ int main()
         
         
     }
-    
-    unsigned int outputNum = num_moves % 1000000007; //problem says to do this because the number of moves is large.
+    unsigned long long int outputNum = fmod(num_moves, 1000000007); //problem says to do this because the number of moves is large.
     
     cout << outputNum << endl;
-    
     return 0;
 }
 
-/*
-int main()
-{
-    int end_vertex, total_paths, current_node, current_target;
-    int current_distance;
-    cin >> end_vertex >>total_paths;
-    nodes.reserve(total_paths);
-    for(int i = 0; i<total_paths; i++)
-    {
-        cin >> current_node >> current_target >> current_distance;
-        
-        Node& this_node = nodes[current_node];
-        this_node.setVertex(current_node);
-        this_node.setNeighbor(current_target,(double)current_distance);
-        
-        Node& other_node = nodes[current_target];
-        other_node.setVertex(current_target);
-        other_node.setNeighbor(current_node,(double)current_distance);
-    }
-    
-    return 0;
-}
-*/
+//18446744073709551615
+//17980131575691243461
+//707474893
